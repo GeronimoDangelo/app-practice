cb0d053f7f12e2bba692f71a5aff45c1863430c7
Dario DAngelo <dariodangelo21@gmail.com>
2023-06-12 10:17:26 -0300

    practice

--- app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
+++ app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
@@ -7,14 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //hola
 
     }
 
 }
 
-class ClaseGenerica<T>(t: T) {
-    val propiedad: T = t
-    fun metodo(param: T) {
-        println(param)
-    }
-}
