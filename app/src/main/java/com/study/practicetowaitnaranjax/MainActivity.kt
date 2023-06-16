ca9687b03924a2f6ab2eb2c4052738d117d06e38
Dario DAngelo <dariodangelo21@gmail.com>
2023-06-16 20:43:14 -0300

    practice 2

--- app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
+++ app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //holaasdafgdfgdfgfdsdas
-
+dsfsdfsdfsdfdsf
     }rgddfgdgfcbcfcfgbfbcfxvxcvcxvdfgdfgdfg
 dfgdg
 }
