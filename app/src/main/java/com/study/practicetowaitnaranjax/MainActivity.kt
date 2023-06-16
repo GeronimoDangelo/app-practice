0d7771e9f14a54e9c1198de07ba441763d0610fb
Dario DAngelo <dariodangelo21@gmail.com>
2023-06-16 20:44:06 -0300

    good practices

--- app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
+++ app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         //holaasdafgasdasdasdsadfgdfgfdsdas
 dsfsdfsdfsdfdsf
     }rgddfgdgfcbcfcfgbfbcfxvxcgdfgdfgdfgdfgdvcxvdfgdfgdfg
-dfgdg
+dfgdgxcvcxvcxvcx
 }
 
