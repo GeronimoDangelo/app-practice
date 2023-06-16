876468a095c3cdccb423c6ac158af13f40154381
Dario DAngelo <dariodangelo21@gmail.com>
2023-06-16 20:53:46 -0300

    good practicesvxc

--- app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
+++ app/src/main/java/com/study/practicetowaitnaranjax/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //holaasdafgasdasdasdsadfgdfgfdsdas
 dsfsdfsdfsdfdsf
-    }rgddfgdgfcbcfcfgbfbcfxvxcgdfgdfgdfgdfgdvcxvdfgdfgdfg
+    }rgddfgdgfcbcfcfgbfbcfxvxcgdfgdfgdfgsdgdfgdfgdfgdfdfgdvcxvdfgdfgdfg
 dfgdgxcvcxvcxvcxxcvxcvxcvxc
 }
 aweweqweqwzxzxfzfzcsdsfsdfsdfds
